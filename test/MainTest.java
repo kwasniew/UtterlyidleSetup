95c675ebe41c690e8d004b0812380cce65fce7ad
Mateusz Kwasniewski <mateusz.kwasniewski@schibsted.pl>
2013-02-04 15:05:06 +0100

    sample test

---
+++ test/MainTest.java
@@ -0,0 +1,15 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class MainTest {
+
+
+   @Test
+   public void testSpike() 
+   {
+      assertEquals("a","a");
+   }
+}
