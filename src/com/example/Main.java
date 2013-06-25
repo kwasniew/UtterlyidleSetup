f4263f5cf0f31c956ca9bb2727a2170f6eba4843
Mateusz Kwasniewski <mateusz.kwasniewski@schibsted.pl>
2013-06-25 11:49:33 +0200

    static file handling

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -2,8 +2,16 @@ package com.example;
 
 import com.googlecode.utterlyidle.BasePath;
 import com.googlecode.utterlyidle.RestApplication;
+import com.googlecode.utterlyidle.dsl.BindingBuilder;
+import com.googlecode.utterlyidle.modules.Module;
 
+import java.net.URL;
+
+import static com.googlecode.totallylazy.URLs.packageUrl;
 import static com.googlecode.utterlyidle.ApplicationBuilder.application;
+import static com.googlecode.utterlyidle.dsl.DslBindings.bindings;
+import static com.googlecode.utterlyidle.dsl.StaticBindingBuilder.in;
+import static com.googlecode.utterlyidle.modules.Modules.bindingsModule;
 
 public class Main extends RestApplication {
     public Main(BasePath basePath) {
@@ -15,6 +23,10 @@ public class Main extends RestApplication {
         if(args.length > 0) {
             port = Integer.parseInt(args[0]);
         }
-        application().addAnnotated(MainResource.class).start(port);
+        application().addAnnotated(MainResource.class).add(staticFileModule(packageUrl(Main.class))).start(port);
+    }
+
+    private static Module staticFileModule(URL url) {
+        return bindingsModule(bindings(in(url).path("")));
     }
 }
